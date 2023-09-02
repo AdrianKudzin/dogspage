b1725427bf50702aad7759c3f4871a33035c32e1
Adrian Kudzin <adrian.kudzin@tlen.pl>
2023-09-02 10:46:25 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-let button = document.querySelector(".header__button");
-let body = document.querySelector(".body");
-let darkMode = document.querySelector(".js-darkMode");
+{
 
-button.addEventListener("click", () => {
-    body.classList.toggle("darkMode")
-    darkMode.innerText === "Włącz" ? darkMode.innerText = "Wyłącz" : darkMode.innerText = "Włącz";
-}) 
+    const welcome = () => {
+        console.log("Witam developerów");
+    };
+
+    const onChangeBackgroundClick = () => {
+        let darkMode = document.querySelector(".js-darkMode");
+        const body = document.querySelector(".body");
+
+        body.classList.toggle("darkMode");
+        darkMode.innerText === "Włącz" ? darkMode.innerText = "Wyłącz" : darkMode.innerText = "Włącz";
+    };
+
+    const init = () => {
+        const button = document.querySelector(".header__button");
+        button.addEventListener("click", onChangeBackgroundClick);
+
+        welcome();
+    };
+
+    init();
+}
